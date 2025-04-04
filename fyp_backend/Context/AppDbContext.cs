ef0d262e98cc7e7d6d7f8036c9047c2cf74328d1
Muhammad-Mak <makmuhammad6@gmail.com>
2025-04-04 18:48:27 +0500

    Added connection string and AppDbContext

---
+++ fyp_backend/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace FYP_Backend.Context
+
+
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
