bef84397cbbec2a49ea24843d78be52bffeef51d
Muhammad-Mak <makmuhammad6@gmail.com>
2025-04-20 16:25:19 +0500

    updated MenuItemDTO to include imageUrl and Description

--- fyp_backend/DTOs/MenuItem/MenuItemDTO.cs
+++ fyp_backend/DTOs/MenuItem/MenuItemDTO.cs
@@ -4,7 +4,9 @@
     {
         public int MenuItemId { get; set; }
         public string Name { get; set; } = null!;
+        public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
         public string CategoryName { get; set; } = null!;
     }
 }
